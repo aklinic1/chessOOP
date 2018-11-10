6905fea62d66b936a50feabd8135f6c05e73092a
ndc1997rpr <aklinic1@etf.unsa.ba>
2018-11-10 15:34:51 +0100

    uradjena metoda move i dodan izuzetak u konstrukotr

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -2,8 +2,9 @@ package ba.unsa.etf.rpr;
 
 public class Bishop extends Chesspiece {
 
-    public Bishop(Color boja, String pozicija){
+    public Bishop(Color boja, String pozicija)throws IllegalChessMoveException{
         super(boja,pozicija);
+        if(!provjeriPoziciju(pozicija)) throw new IllegalChessMoveException("Ilegalna pozicija");
     }
     @Override
     public String getPosition(){
@@ -14,4 +15,27 @@ public class Bishop extends Chesspiece {
     public Color getColor() {
         return boja;
     }
+    @Override
+    public void move(String position) throws IllegalChessMoveException{
+        char slovo = position.charAt(0);
+        int broj = position.charAt(1) - '0';
+
+        if(!provjeriPoziciju(position)){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        char slovo_p = pozicija.charAt(0);
+        int broj_p = pozicija.charAt(1) - '0';
+
+        if(slovo == slovo_p && broj == broj_p){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        
+        if(broj_p - broj == slovo_p - slovo){
+            pozicija = position;
+        }
+        else{
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+    }
+
 }
