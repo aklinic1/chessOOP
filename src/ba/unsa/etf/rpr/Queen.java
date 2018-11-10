378839712b136610776ffec2d209f3284a5a27c9
ndc1997rpr <aklinic1@etf.unsa.ba>
2018-11-10 15:21:22 +0100

    uradjena metoda move i dodan izuzetak u konstrukotru

--- src/ba/unsa/etf/rpr/Queen.java
+++ src/ba/unsa/etf/rpr/Queen.java
@@ -2,8 +2,9 @@ package ba.unsa.etf.rpr;
 
 public class Queen extends Chesspiece {
 
-    public Queen(Color boja, String pozicija){
+    public Queen(Color boja, String pozicija) throws IllegalChessMoveException{
         super(boja,pozicija);
+        if(!provjeriPoziciju(pozicija)) throw new IllegalChessMoveException("Ilegalna pozicija");
     }
 
     @Override
@@ -14,5 +15,25 @@ public class Queen extends Chesspiece {
     @Override
     public Color getColor() {
         return boja;
+    }
+    public void move(String position) throws IllegalChessMoveException{
+        char slovo = position.charAt(0);
+        int broj = position.charAt(1) - '0';
+
+        if(!provjeriPoziciju(position)){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        char slovo_p = pozicija.charAt(0);
+        int broj_p = pozicija.charAt(1) - '0';
+
+        if(slovo == slovo_p && broj == broj_p){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        if(slovo == slovo_p || broj == broj_p || Math.abs(slovo - slovo_p ) == Math.abs(broj - broj_p)){
+            pozicija = position;
+        }
+        else {
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
     }
 }
