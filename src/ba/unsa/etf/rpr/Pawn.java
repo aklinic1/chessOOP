58f6d7b32eea3d9becfe7ac1ed3192256032d005
ndc1997rpr <aklinic1@etf.unsa.ba>
2018-11-10 15:41:19 +0100

    uradjena metoda move i dodan izuzetak u konstrukotr

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -2,8 +2,9 @@ package ba.unsa.etf.rpr;
 
 public class Pawn extends Chesspiece {
 
-    public Pawn(Color boja, String pozicija){
+    public Pawn(Color boja, String pozicija) throws IllegalChessMoveException{
         super(boja,pozicija);
+        if(!provjeriPoziciju(pozicija)) throw new IllegalChessMoveException("Ilegalna pozicija");
     }
     @Override
     public String getPosition(){
@@ -13,5 +14,27 @@ public class Pawn extends Chesspiece {
     @Override
     public Color getColor() {
         return boja;
+    }
+    @Override
+    public void move(String position) throws IllegalChessMoveException{
+        char slovo = position.charAt(0);
+        int broj = position.charAt(1) - '0';
+
+        if (!provjeriPoziciju(position)){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        char slovo_p = pozicija.charAt(0);
+        int broj_p = pozicija.charAt(1) - '0';
+
+        if(slovo == slovo_p && broj == broj_p){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+
+        if(slovo == slovo_p && broj == broj_p - 1){
+            pozicija = position;
+        }
+        else {
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
     }
 }
