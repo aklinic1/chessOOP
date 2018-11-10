c848efa42c1685b2203b9ace3814f7bd324d6d7b
ndc1997rpr <aklinic1@etf.unsa.ba>
2018-11-10 15:26:34 +0100

    uradjena metoda move i dodan izuzetak u konstrukotru, popravljena metoda getPosition()

--- src/ba/unsa/etf/rpr/Rook.java
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -2,16 +2,35 @@ package ba.unsa.etf.rpr;
 
 public class Rook extends Chesspiece{
 
-    public Rook(Color boja, String pozicija){
+    public Rook(Color boja, String pozicija) throws IllegalChessMoveException{
         super(boja,pozicija);
+        if(!provjeriPoziciju(pozicija)) throw new IllegalChessMoveException("Ileglana pozicija");
     }
     @Override
     public String getPosition(){
-        return boja;
+        return pozicija;
     }
 
     @Override
     public Color getColor() {
         return boja;
+    }
+    @Override
+    public void move(String position) throws IllegalChessMoveException{
+        char slovo = position.charAt(0);
+        int broj = position.charAt(1) - '0';
+
+        if (!provjeriPoziciju(position)) {
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        char slovo_p = pozicija.charAt(0);
+        int broj_p = pozicija.charAt(1) - '0';
+
+        if (slovo == slovo_p || broj == broj_p){
+            pozicija = position;
+        }
+        else{
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
     }
 }
