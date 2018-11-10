c3b6acda0231307e04781ee0719aeb35671b7785
ndc1997rpr <aklinic1@etf.unsa.ba>
2018-11-10 15:12:16 +0100

    popravljen konstruktor da baca izuzetak

--- src/ba/unsa/etf/rpr/Chesspiece.java
+++ src/ba/unsa/etf/rpr/Chesspiece.java
@@ -6,14 +6,30 @@ public abstract class Chesspiece {
     protected Color boja;
     protected String pozicija;
 
-    public Chesspiece(Color boja, String pozicija){
+    public Chesspiece(Color boja, String pozicija) throws IllegalChessMoveException{
         this.boja = boja;
         this.pozicija = pozicija;
+    };
+    public boolean provjeriPoziciju(String pozicija){
+
+        if(pozicija.length() > 2) return false;
+
+        int broj = pozicija.charAt(1) - '0';
+        if(broj < 1 || broj > 8) {
+
+            return false;
+        }
+        char slovo = pozicija.charAt(0);
+        if(((int)slovo < 65 || (int)slovo > 72) || ((int)slovo < 97 || (int)slovo > 104 )) {
+            return false;
+        }
+
+        return true;
     }
     public abstract String getPosition();
 
     public abstract Color getColor();
 
-    public abstract void move(String position);
+    public abstract void move(String position) throws IllegalChessMoveException;
 
 }
