d8a7a85307bc5e9e3ce941bc0db47126a82c163c
ndc1997rpr <aklinic1@etf.unsa.ba>
2018-11-10 15:11:25 +0100

    uradjena metoda move i dodan izuzetak u konstrukotru

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -2,9 +2,11 @@ package ba.unsa.etf.rpr;
 
 public class King extends Chesspiece{
 
-    public King(Color boja, String pozicija){
-        super(boja,pozicija);
+    public King(Color boja, String pozicija) throws IllegalChessMoveException{
+        super(boja, pozicija);
+        if(!provjeriPoziciju(pozicija)) throw new IllegalChessMoveException("Ilegalna pozicija");
     }
+
     @Override
     public String getPosition(){
         return pozicija;
@@ -13,6 +15,32 @@ public class King extends Chesspiece{
     @Override
     public Color getColor() {
         return boja;
+    }
+
+    @Override
+    public void move(String position) throws IllegalChessMoveException{
+        char slovo = position.charAt(0);
+        int broj = position.charAt(1) - '0';
+
+        if(!provjeriPoziciju(position)){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        char slovo_p = pozicija.charAt(0);
+        int broj_p = pozicija.charAt(1) - '0';
+
+        if(slovo == slovo_p && broj == broj_p){
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+
+
+        if((slovo == slovo_p || slovo == slovo_p + 1 || slovo == slovo_p - 1) &&
+                (broj == broj_p + 1 || broj == broj_p - 1 || broj == broj_p)) {
+            pozicija = position;
+        }
+        else{
+            throw new IllegalChessMoveException("Ilegalan potez");
+        }
+        pozicija = position;
     }
 
 
